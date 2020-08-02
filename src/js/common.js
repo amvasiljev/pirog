0a9a1ee9ab139b3d61905aa42ca6c1406eb52f83
bikeol <a@mosam.ru>
2020-08-02 13:55:47 +0300

    src:

--- src/js/common.js
+++ src/js/common.js
@@ -6,12 +6,12 @@ $(function () {
 
 
 
-$('.preview').each(function(indx){
+$('.preview').each(function (indx) {
   $(this).hover(
-    function(){
-     $(this).find('.button').addClass('button_visible')
+    function () {
+      $(this).find('.button').addClass('button_visible')
     },
-    function(){
+    function () {
       $(this).find('.button').removeClass('button_visible')
     }
   )
@@ -43,17 +43,24 @@ $('.tabs').on('click', 'a', function (e) {
 
 
 
-$('.order__input').on('focus', function(){
+$('.order__input').on('focus', function () {
   $(this).addClass('order__input_active');
 })
-$('.order__input').on('blur', function(){
-  $(this).removeClass('order__input_active');
+$('.order__input').on('blur', function () {
+  if (!$(this).val()) {
+    $(this).removeClass('order__input_active');
+  }
+
+
 })
-$('.order__textarea').on('focus', function(){
+$('.order__textarea').on('focus', function () {
   $(this).addClass('order__textarea_active');
 })
-$('.order__textarea').on('blur', function(){
-  $(this).removeClass('order__textarea_active');
+$('.order__textarea').on('blur', function () {
+  if (!$(this).val()) {
+    $(this).removeClass('order__textarea_active');
+  }
+
 })
 
 
@@ -68,15 +75,12 @@ $('.order__input_phone').mask('+7 (000) 000-00-00', {
 //mobile menu
 
 
-// products
+
 // if (/Android|webOS|iPhone|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-//   var product = $('.product');
-//   product.each(function (indx) {
-//     if (indx >= 3) $(this).hide()
-//   })
+//  
 // }
 
-//products
+
 
 
 
@@ -107,8 +111,6 @@ $('.slider__box_foto').slick({
       breakpoint: 480,
       settings: {
         slidesToShow: 2,
-        // nextArrow: '<img class="slider__arrow slider__arrow_next" src="/assets/img/arrow_next_mobile.png" alt="">',
-        // prevArrow: '<img class="slider__arrow slider__arrow_prev" src="/assets/img/arrow_prev_mobile.png" alt="">',
       }
     }
   ]
@@ -138,8 +140,6 @@ $('.slider__box_numbers').slick({
       breakpoint: 480,
       settings: {
         slidesToShow: 2,
-        // nextArrow: '<img class="slider__arrow slider__arrow_next" src="/assets/img/arrow_next_mobile.png" alt="">',
-        // prevArrow: '<img class="slider__arrow slider__arrow_prev" src="/assets/img/arrow_prev_mobile.png" alt="">',
       }
     }
   ]
@@ -169,8 +169,36 @@ $('.slider__box_3d').slick({
       breakpoint: 480,
       settings: {
         slidesToShow: 2,
-        // nextArrow: '<img class="slider__arrow slider__arrow_next" src="/assets/img/arrow_next_mobile.png" alt="">',
-        // prevArrow: '<img class="slider__arrow slider__arrow_prev" src="/assets/img/arrow_prev_mobile.png" alt="">',
+      }
+    }
+  ]
+});
+
+$('.slider__box_gallery').slick({
+  lazyLoad: 'ondemand',
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  nextArrow: '<div class="slider__arrow slider__arrow_next"></div>',
+  prevArrow: '<div class="slider__arrow slider__arrow_prev"></div>',
+  appendArrows: $('.slider__arrows_gallery'),
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
       }
     }
   ]
@@ -181,24 +209,24 @@ $('.slider__box_3d').slick({
 // end slider
 
 
-$('#file').on('change', function() {
+$('#file').on('change', function () {
   var arrayFiles = this.files, // массив с выбранными файлами
-      formItem = this.parentNode, // родительский элемент, для того чтобы вставить список с файлами
-      listFiles = document.createElement('ul'), // список с файлами
-      li = ''; // файлы
-  
+    formItem = this.parentNode, // родительский элемент, для того чтобы вставить список с файлами
+    listFiles = document.createElement('ul'), // список с файлами
+    li = ''; // файлы
+
   // Если список с файлами уже вставлен в ДОМ, то удаляем его
   if (formItem.querySelector('.order__list')) {
     formItem.querySelector('.order__list').remove();
   }
-  
+
   listFiles.className = 'order__list'; // 
-  
+
   for (var i = 0; i < arrayFiles.length; i++) {
     li += '<li>' + arrayFiles[i].name + '</li>'; // <li>Имя файла</li>
   }
-  
+
   listFiles.innerHTML = li;
-  
-  formItem.appendChild(listFiles);  
+
+  formItem.appendChild(listFiles);
 });
